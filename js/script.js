a4104bb4a1af42c6a73f668d92ce251ccb305e62
Mahmoud-AlHajHasan <alhajhasanmahmoud99@gmail.com>
2022-09-25 16:14:41 +0300

    Create game() that loops the round five times and keeps a score for both

--- js/script.js
+++ js/script.js
@@ -41,6 +41,48 @@ function playRound(playerChoice, computerChoice){
   return winner;
 }
 
+// make a function that prompts input and saves it playerChoice to use for the playRound function
+function getPlayerChoice(){
+  let playerChoice = prompt(`Rock, Paper or Scissors?`).toLowerCase()
+  return playerChoice
+}
+
+
 // start a loop with a games counter (games), increment by 1 every time unless a draw
 // playerPoints = playerPoints + 1 if player wins
-// 
+// computerPoints = computerPoints + 1 if computer wins
+// when loop exits 
+// if points > points totalWin = `Player`
+// if else totalWIn = `Computer`
+// show message depending on totalWin
+
+function game() {
+  let computerPoints = 0;
+  let playerPoints = 0;
+  let totalWinner;
+  for (i=0;i<5;i++){
+    let computerChoice = getComputerChoice()
+    let playerChoice = getPlayerChoice()
+    let winner = playRound(playerChoice,computerChoice)
+    if (winner === `computer`){
+      computerPoints += 1
+      alert(`YOU LOST`)
+    } else if (winner === `player`) {
+      playerPoints += 1
+      alert(`You win this round!`)
+    } else { 
+      alert(`Draw!`)
+      i -= 1
+    }
+  }
+  if (playerPoints > computerPoints) {
+    alert(`YOU WON IT ALL GZ!!!!`)
+  } else {
+    alert(`lol loser.`)
+  }
+}
+
+game()
+
+// game starts automatically on refresh, asks for input of playerChoice then measures it against computerChoice and return in an alert the result.
+// game loops for five finished rounds, and keeps points for each player and the highest points win the game in the end.
