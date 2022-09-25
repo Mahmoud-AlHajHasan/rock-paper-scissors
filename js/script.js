56830101ac2e059d2e9083812da7e9ad30414b1d
Mahmoud-AlHajHasan <alhajhasanmahmoud99@gmail.com>
2022-09-25 15:37:25 +0300

    Create a function that returns the winner of two choices

--- js/script.js
+++ js/script.js
@@ -14,3 +14,33 @@ function getComputerChoice() {
   }
   return computerChoice
 }
+
+
+
+// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+// 
+
+function playRound(playerChoice, computerChoice){
+  let winner;
+  // computer winner 
+  if ((playerChoice === `rock` && computerChoice === `paper`) || (playerChoice === `paper` && computerChoice === `scissors`) || (playerChoice === `scissors` && computerChoice === `rock`)) {
+    winner = `computer`
+    return winner;
+  } else if ((playerChoice === `paper` && computerChoice === `rock`) || (playerChoice === `scissors` && computerChoice === `paper`) || (playerChoice === `rock` && computerChoice === `scissors`)) {
+    winner = `player`
+    return winner;
+  } else if (computerChoice === playerChoice) {
+    winner = `none`
+    return winner;
+  } else { 
+    winner = `none`
+    return winner;
+  }
+
+
+  return winner;
+}
+
+// start a loop with a games counter (games), increment by 1 every time unless a draw
+// playerPoints = playerPoints + 1 if player wins
+// 
