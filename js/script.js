c6adf137233b0c880379bbfe5054f855a4293181
Mahmoud-AlHajHasan <alhajhasanmahmoud99@gmail.com>
2022-09-26 13:38:30 +0300

    Make code easier to read & remove comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// get a random number from 1 to 3 and assign it to a variable
-// assign rock paper or scissor to computerChoice depending on the random number generated
-// return computerChoice
-
 function getComputerChoice() {
   let choiceValue = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
   let computerChoice;
@@ -15,51 +11,35 @@ function getComputerChoice() {
   return computerChoice
 }
 
-
-
-// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-// 
-
 function playRound(playerChoice, computerChoice){
   let winner;
-  // computer winner 
-  if ((playerChoice === `rock` && computerChoice === `paper`) || (playerChoice === `paper` && computerChoice === `scissors`) || (playerChoice === `scissors` && computerChoice === `rock`)) {
-    winner = `computer`
+  if ((playerChoice === `rock` && computerChoice === `paper`)
+    || (playerChoice === `paper` && computerChoice === `scissors`)
+    || (playerChoice === `scissors` && computerChoice === `rock`)) {
+    winner = `computer`;
     return winner;
-  } else if ((playerChoice === `paper` && computerChoice === `rock`) || (playerChoice === `scissors` && computerChoice === `paper`) || (playerChoice === `rock` && computerChoice === `scissors`)) {
-    winner = `player`
+  } else if ((playerChoice === `paper` && computerChoice === `rock`)
+    || (playerChoice === `scissors` && computerChoice === `paper`)
+    || (playerChoice === `rock` && computerChoice === `scissors`)) {
+    winner = `player`;
     return winner;
   } else if (computerChoice === playerChoice) {
-    winner = `none`
+    winner = `none`;
     return winner;
   } else { 
-    winner = `none`
+    winner = `none`;
     return winner;
   }
-
-
-  return winner;
 }
 
-// make a function that prompts input and saves it playerChoice to use for the playRound function
 function getPlayerChoice(){
   let playerChoice = prompt(`Rock, Paper or Scissors?`).toLowerCase()
   return playerChoice
 }
 
-
-// start a loop with a games counter (games), increment by 1 every time unless a draw
-// playerPoints = playerPoints + 1 if player wins
-// computerPoints = computerPoints + 1 if computer wins
-// when loop exits 
-// if points > points totalWin = `Player`
-// if else totalWIn = `Computer`
-// show message depending on totalWin
-
 function game() {
   let computerPoints = 0;
   let playerPoints = 0;
-  let totalWinner;
   for (i=0;i<5;i++){
     let computerChoice = getComputerChoice()
     let playerChoice = getPlayerChoice()
@@ -83,6 +63,3 @@ function game() {
 }
 
 game()
-
-// game starts automatically on refresh, asks for input of playerChoice then measures it against computerChoice and return in an alert the result.
-// game loops for five finished rounds, and keeps points for each player and the highest points win the game in the end.
